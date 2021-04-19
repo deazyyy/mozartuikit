b55b4160da2886f9678620b83a045a4413395f66
deazyyy <d.s@vimerco.com>
2021-04-19 23:58:15 +0530

    first

---
+++ src/components/Button/theme.ts
@@ -0,0 +1,56 @@
+import { scales, variants } from "./types";
+
+export const scaleVariants = {
+  [scales.MD]: {
+    height: "auto",
+    padding: "14px 28px",
+  },
+  [scales.SM]: {
+    height: "auto",
+    padding: "14px 28px",
+  },
+  [scales.XS]: {
+    height: "auto",
+    fontSize: "12px",
+    padding: "14px 28px",
+  },
+};
+
+export const styleVariants = {
+  [variants.PRIMARY]: {
+    backgroundColor: "primary",
+    color: "white",
+  },
+  [variants.SECONDARY]: {
+    backgroundColor: "transparent",
+    border: "2px solid",
+    borderColor: "primary",
+    boxShadow: "none",
+    color: "primary",
+    ":disabled": {
+      backgroundColor: "transparent",
+    },
+  },
+  [variants.TERTIARY]: {
+    backgroundColor: "tertiary",
+    boxShadow: "none",
+    color: "primary",
+  },
+  [variants.SUBTLE]: {
+    backgroundColor: "textSubtle",
+    color: "white",
+  },
+  [variants.DANGER]: {
+    backgroundColor: "failure",
+    color: "white",
+  },
+  [variants.SUCCESS]: {
+    backgroundColor: "success",
+    color: "white",
+  },
+  [variants.TEXT]: {
+    backgroundColor: "transparent",
+    color: "primary",
+    boxShadow: "none",
+  },
+};
